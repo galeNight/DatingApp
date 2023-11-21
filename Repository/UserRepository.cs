f86f09b8a2d438b9a7b1976c7205e032c2c79848
Jannick Mørup <jannickmorup@gmail.com>
2023-11-21 14:07:43 +0100

    .

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -316,7 +316,7 @@ namespace DatingApp.Repository
             }
         }
         // methode add Like
-        public void AddLike(int liker,int likee)// 
+        public void AddLike(int liker,int likee)// conflit foreign key
         {
             using(SqlConnection conn =new SqlConnection(_connstring))
             {
